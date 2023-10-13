1e692a80c1213caaed08fec22018f6f8f37629d5
C <44219557+1152987042@users.noreply.github.com>
2023-10-13 15:01:29 +0800

    Update FeignConfig.java

--- FeignConfig.java
+++ FeignConfig.java
@@ -16,10 +16,7 @@ import org.springframework.context.annotation.Configuration;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * @author zhaodahai
- * 2021/8/25
- */
+
 @Configuration
 @Slf4j
 public class FeignConfig {
